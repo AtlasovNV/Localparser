c8fd3c8ed75fb4e2224190f25cd90489e27f0db7
devqwil <51747787+devqwil@users.noreply.github.com>
2019-08-27 10:04:46 +0300

    Парсер

---
+++ parser.py
@@ -0,0 +1,27 @@
+import requests
+from bs4 import BeautifulSoup as bs
+import time
+
+#для нового пк нужно изменить
+#путь до интерпретатора
+#имя пользователя
+#путь до проекта
+
+headers = {'accept': '*/*',
+                     'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) snap Chromium/76.0.3809.87 Chrome/76.0.3809.87 Safari/537.36'}
+base_url = 'https://www.cbr.ru/'
+
+def cbrf_pars(base_url, headers):
+    session = requests.session()
+    request = session.get(base_url, headers=headers)
+    if request.status_code == 200:
+        print('join')
+        soup = bs(request.content, 'html.parser')
+        div = soup.find_all('div', attrs={'id':'widget_exchange'})
+        print(div)
+        file = open('/home/qwil/devlop/projects/parser/Currency Rates_{}.txt'.format(time.ctime()), "w")
+        file.write(str(div))
+        file.close()
+    else:
+        print('error 404 page not found')
+cbrf_pars(base_url, headers)
