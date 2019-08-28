6bb656756e8d6b840ecedf07ae3b71fef2f65804
devqwil <51747787+devqwil@users.noreply.github.com>
2019-08-28 22:40:06 +0300

    Update parser.py

--- parser.py
+++ parser.py
@@ -1,33 +1,49 @@
+
 #!/home/qwil/Localparser/venv/bin/python
 
 import requests
 from bs4 import BeautifulSoup as bs, BeautifulSoup
 import time
 
-headers = {'accept': '*/*',
-                     'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) snap Chromium/76.0.3809.87 Chrome/76.0.3809.87 Safari/537.36'}
-base_url = 'https://www.cbr.ru/'
 
-def cbrf_pars(base_url, headers):
+def find_table_rate():
+    headers = {'accept': '*/*',
+               'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) snap Chromium/76.0.3809.87 Chrome/76.0.3809.87 Safari/537.36'}
+    base_url = 'https://www.cbr.ru/'
     session = requests.session()
     request = session.get(base_url, headers=headers)
     if request.status_code == 200:
         print('join')
         soup = bs(request.content, 'html.parser')
-        div = soup.find_all('div', attrs={'id':'widget_exchange'})
-        print(div)
-        file = open('/home/qwil/Localparser/pars.txt', "w")
-        file.write(str(div))
+        divi = soup.find_all('div', attrs={'id':'widget_exchange'})
+        return divi
+
+def save_rates():
+    # -*- coding: utf-8 -*-
+    file = open('pars.txt', "w")
+    file.write(str(find_table_rate()))
+    file.close()
+save_rates()
+
+def format_html():
+    # -*- coding: utf-8 -*-
+    with open('pars.txt', 'r') as file:
+        html = file.read()
+    result = ''.join(BeautifulSoup(html).findAll(text=True))
+    file = open('pars.txt', "w")
+    file.write(str(result))
+    file.close()
+format_html()
+
+def format_rate():
+    # -*- coding: utf-8 -*-
+    with open('pars.txt') as file:
+        course = [row.strip() for row in file]
+        dol= 'Курс доллара \n', course[18]
+        evr = 'Курс евро \n', course[29]
+        file = open('Currency Rates_{}.txt'.format(time.ctime()), "w")
+        file.write(str(dol))
+        file.write('\n')
+        file.write(str(evr))
         file.close()
-        with open('/home/qwil/Localparser/pars.txt', 'r') as file:
-            html = file.read()
-        result = ''.join(BeautifulSoup(html).findAll(text=True))
-        file = open('/home/qwil/Localparser/Currency Rates_{}.txt'.format(time.ctime()), "w")
-        file.write(str(result))
-        file.close()
-    else:
-        print('error 404 page not found')
-cbrf_pars(base_url, headers)
-
-
-
+format_rate()
